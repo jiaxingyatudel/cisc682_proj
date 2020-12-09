4def21df7d5e3dfe06100fb10998abcd30e3ff22
Jiaxing Yang <jiaxingy@udel.edu>
2020-12-09 14:03:12 -0500

    typo

--- web/recipe_like_btn.vue
+++ web/recipe_like_btn.vue
@@ -4,7 +4,7 @@
 <template>
     <span>
         <span v-if="!recipe_info.post_like" class="card-link" v-on:click="click_like_post()"><i class="fa fa-heart-o" aria-hidden="true"></i>&nbsp;Like this recipe to save for later</span>
-        <span v-if="recipe_info.post_like" class="card-link" v-on:click="click_cancel_like_post()"><i class="fa fa-heart" aria-hidden="true"></i>&nbsp;Cacel like</span>
+        <span v-if="recipe_info.post_like" class="card-link" v-on:click="click_cancel_like_post()"><i class="fa fa-heart" aria-hidden="true"></i>&nbsp;Cancel like</span>
     </span>
 </template>
 
